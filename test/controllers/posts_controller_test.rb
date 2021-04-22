b93f807ee4a46774015daf321a6140ceaab88b1e
Grigori <grigori.aramyan@gmail.com>
2021-04-22 13:34:04 +0400

    add create post endpoint tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,19 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create post" do
+    assert_difference("Post.count") do
+      post posts_url, params: { post: { title: "test title" } }, as: :json
+    end
+
+    assert_response :success
+  end
+
+  test "should not create post without required title" do
+    post posts_url, params: { post: { text: "test body text" } }, as: :json
+
+    assert_response 400
+  end
+
 end
