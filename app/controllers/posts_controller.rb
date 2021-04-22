95835ee091286ecab7144dbca5ee07642eb95672
Grigori <grigori.aramyan@gmail.com>
2021-04-22 08:07:30 +0400

    add create action in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
   def create
-    msg = {:name => 'john', :age => 21}
-    render :json => msg
+    new_post = Post.new(post_params)
+
+    if new_post.save
+      render :json => { :msg => 'created', :post => new_post }, :status => 201
+    else
+      render :json => { :msg => 'could not save post', :post => nil }, :status => 400
+    end
+
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
