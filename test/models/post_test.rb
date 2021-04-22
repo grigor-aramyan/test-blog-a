5f348f6fa4d290a81859adbbb424ed3c8584fab7
Grigori <grigori.aramyan@gmail.com>
2021-04-22 08:57:16 +0400

    unit tests for post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,16 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Should not save post without title" do
+    new_post = Post.new
+
+    assert_not new_post.save, "Saving the post without title"
+  end
+
+  test "Should save post with required fields" do
+    new_post = Post.new({ :title => "test title" })
+
+    assert new_post.save, "Not saving post with required fields in place"
+  end
 end
